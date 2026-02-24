29107abe2b04a99a4f81d7bc51c751ed652b1ef5
Twilight Techie <da.kings.air@gmail.com>
2026-02-24 23:23:32 +0100

    feat: Implement Next.js authentication middleware for route protection and a root layout with AuthProvider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 import { ReactNode } from 'react'
 import AuthProvider from '@/components/AuthProvider'
 
+export const viewport: Viewport = {
+  themeColor: '#0b512a',
+}
+
 export const metadata: Metadata = {
   title: 'MSSN RAMS',
   description: 'MSSN LASU Epe Attendance Management System',
-  themeColor: '#0b512a',
 }
+
 
 export default function RootLayout({
   children,
